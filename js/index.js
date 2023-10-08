d0f98d1df1e448905da9b470fca9d8bd259927a0
MariaKundrat <03032005km@gmail.com>
2023-10-08 23:57:33 +0300

    Add lab4 to the repo

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ function renderSmartphones(smartphones) {
     const phonesList = document.getElementById('phones-list');
     phonesList.innerHTML = '';
 
-    smartphones.forEach(phone => {
+    smartphones.forEach((phone, index) => {
         const phoneItem = document.createElement('div');
         phoneItem.classList.add('phone-item');
         phoneItem.innerHTML = `
@@ -37,10 +37,26 @@ function renderSmartphones(smartphones) {
             <p>Бренд: ${phone.brand}</p>
             <p>Модель: ${phone.model}</p>
             <p>Ціна: $${phone.price}</p>
+            <button class="edit-button" data-index="${index}">Редагувати</button>
         `;
         phonesList.appendChild(phoneItem);
     });
-    filteredSmartphones = smartphones;
+
+    phonesList.querySelectorAll('.edit-button').forEach((editButton) => {
+        editButton.addEventListener('click', () => {
+            const index = editButton.getAttribute('data-index');
+            openEditModal(smartphones[index]);
+        });
+    });
+}
+
+function openEditModal(phone) {
+    const editForm = document.getElementById('edit-form');
+    editForm.style.display = 'block';
+
+    document.getElementById('edit-brand').value = phone.brand;
+    document.getElementById('edit-model').value = phone.model;
+    document.getElementById('edit-price').value = phone.price;
 }
 
 renderSmartphones(filteredSmartphones);
@@ -80,3 +96,99 @@ function calculateTotalPrice() {
 }
 
 calculateTotalPrice();
+
+const createButton = document.getElementById("create-button");
+const createForm = document.getElementById("create-form");
+
+createButton.addEventListener("click", () => {
+    createForm.style.display = "block";
+});
+
+const cancelCreateButton = document.getElementById("cancel-create-button");
+
+cancelCreateButton.addEventListener("click", () => {
+    createForm.style.display = "none";
+});
+
+createForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const brand = document.getElementById("brand").value;
+    const model = document.getElementById("model").value;
+    const price = parseFloat(document.getElementById("price").value);
+
+    const newSmartphone = new Smartphone(brand, model, price);
+
+    smartphonesData.push(newSmartphone);
+
+    createForm.reset();
+
+    createForm.style.display = "none";
+
+    renderSmartphones(smartphonesData);
+    calculateTotalPrice();
+});
+
+const editForm = document.getElementById("edit-form");
+const editButton = document.getElementById("edit-button");
+const cancelButton = document.getElementById("cancel-button");
+
+document.getElementById('edit-button').addEventListener('click', () => {
+    const phonesList = document.getElementById('phones-list');
+    phonesList.innerHTML = '';
+
+    smartphonesData.forEach((phone, index) => {
+        const phoneItem = document.createElement('div');
+        phoneItem.classList.add('phone-item');
+        phoneItem.innerHTML = `
+            <img src="${phone.image}" alt="${phone.brand} ${phone.model}" width="200">
+            <p>Бренд: ${phone.brand}</p>
+            <p>Модель: ${phone.model}</p>
+            <p>Ціна: $${phone.price}</p>
+            <button class="edit-button" data-index="${index}">Редагувати</button>
+        `;
+        phonesList.appendChild(phoneItem);
+    });
+
+    phonesList.querySelectorAll('.edit-button').forEach((editButton) => {
+        editButton.addEventListener('click', () => {
+            const index = editButton.getAttribute('data-index');
+        });
+    });
+});
+
+createButton.addEventListener("click", () => {
+    createForm.style.display = "block";
+    editForm.style.display = "none";
+});
+
+createForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const brand = document.getElementById("brand").value;
+    const model = document.getElementById("model").value;
+    const price = parseFloat(document.getElementById("price").value);
+    const color = document.getElementById("color").value;
+    const image = document.getElementById("image").value;
+
+    const newSmartphone = new Smartphone(brand, model, price, color, image);
+
+    smartphonesData.push(newSmartphone);
+
+    createForm.reset();
+
+    renderSmartphones(filteredSmartphones);
+    calculateTotalPrice();
+});
+
+editButton.addEventListener("click", () => {
+    createForm.style.display = "none";
+    editForm.style.display = "block";
+});
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+cancelButton.addEventListener("click", () => {
+    createForm.style.display = "none";
+    editForm.style.display = "none";
+});
